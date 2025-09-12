a9f5b13d406cfbd27ea29314757a88da0e4f401b
Mayur Ray <shiremen@gmail.com>
2025-09-12 15:45:13 +0530

    vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
-        start_url: '/',
+        start_url: '/crm-contact-page/',
         icons: [
           {
             src: 'pwa-192x192.png',
